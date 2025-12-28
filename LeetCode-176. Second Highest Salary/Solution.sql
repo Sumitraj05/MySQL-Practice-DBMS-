ae551ff9fb2a24614f024e70547d2da391118077
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2025-12-29 00:34:08 +0530

    Create Solution.sql

---
+++ LeetCode-176. Second Highest Salary/Solution.sql
@@ -0,0 +1,24 @@
+-- Methon 1 - using Limit and Offset
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
+-- Method 2 - Using Max function and Subquery
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+-- Method 3 - Using DENSE_ROW function 
+SELECT (
+    SELECT DISTINCT salary
+    FROM (
+        SELECT 
+            salary,
+            DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+        FROM Employee
+    ) AS RankedSalaries
+    WHERE rnk = 2
+) AS SecondHighestSalary;
